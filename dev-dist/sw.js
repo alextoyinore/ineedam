7f330c789f87f836b663c6f67f9743babad86709
Alexander Ore <aore8030@gmai.com>
2026-03-02 12:22:00 +0100

    removed mobile app

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,8 +78,11 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
+    "url": "registerSW.js",
+    "revision": "3ca0b8505b4bec776b69afdba2768812"
+  }, {
     "url": "index.html",
-    "revision": "0.6eqmellq1og"
+    "revision": "0.kl9c61tf5t"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- mobile-app/src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import 'react-native-url-polyfill/auto';
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://owlkmvgpuzwnpaqigfuf.supabase.co';
-const supabaseAnonKey = 'sb_publishable_r0kouvyQVn5fxMDmTQfbDA_v8_7IDE3';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
