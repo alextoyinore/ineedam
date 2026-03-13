99873706c6efe6e979057fc5689c57bf240e9ef6
Alexander Ore <aore8030@gmai.com>
2026-03-13 15:32:08 +0100

    updated welcome page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -50,7 +50,6 @@ export const LandingPage = () => {
                     justifyContent: 'center',
                     cursor: 'pointer',
                     color: 'var(--text-primary)',
-                    boxShadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1)',
                     transition: 'all 0.2s'
                 }}
                 className="nav-link-hover"
